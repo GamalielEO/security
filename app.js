1f2114b6f1b30460440cba7dbb9d6764d6f8d356
GamalielEO <gamalielerimi@gmail.com>
2023-04-22 16:42:12 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,78 @@
+//jshint esversion:6 
+  require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String
+});
+
+const secretkey = process.env.SECRET ;
+userSchema.plugin(encrypt,{secret:secretkey ,encryptedFields:["password"] });
+
+const User = new mongoose.model("User",userSchema);
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.get("/submit", function (req, res) {
+  res.render("submit");
+});
+
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser
+    .save()
+    .then((result) => {
+      res.render("secrets");
+      // Handle the results of the query here
+    })
+    .catch((err) => {
+      console.log(err);
+      // Handle any errors that occurred during the query
+    });
+});
+
+app.post("/login",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;  
+    
+    User.findOne({email:username})
+    .then((result) => {
+        if(password===result.password)
+        res.render("secrets");
+        
+        // Handle the results of the query here
+      })
+      .catch((err) => {
+        console.log(err);
+        // Handle any errors that occurred during the query
+      });
+
+})
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
